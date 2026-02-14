073e4cb685199b6913e3a291787803ea8523b88d
Eduardo <eduardovcn74@gmail.com>
2026-02-14 19:11:12 -0300

    Primeiro commit da aplicação.

---
+++ src/main/java/com/deposito/construcao_manager/ConstrucaoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.deposito.construcao_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstrucaoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstrucaoManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deposito/construcao_manager/ConstrucaoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deposito.construcao_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstrucaoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
