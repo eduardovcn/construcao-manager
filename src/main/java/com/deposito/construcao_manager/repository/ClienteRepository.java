5ab12de0a07ee21bd1d3504406288f34ecc8c286
Eduardo <eduardovcn74@gmail.com>
2026-02-18 14:23:11 -0300

    fix: Mudando a implementação da geração do banco de dados para o H2. Fins de teste

--- src/main/java/com/deposito/construcao_manager/repository/ClienteRepository.java
+++ src/main/java/com/deposito/construcao_manager/repository/ClienteRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
-        List<Cliente> findByNomeContainingIgnoreCase(String nome);
+        List<Cliente> findByNomeCompletoContainingIgnoreCase(String nome);
 
        Optional <Cliente> findById(Long clienteId);
 }
