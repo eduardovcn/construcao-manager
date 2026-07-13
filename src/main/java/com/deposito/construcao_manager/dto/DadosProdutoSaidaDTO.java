f193aca199a1a33db933a7160cdd29b7477420f5
eduardo <eduardovcn74@gmail.com>
2026-07-13 14:26:41 -0300

    fix: Ajustando o DTO para record, para garantir a imutabilidade das variáveis

--- src/main/java/com/deposito/construcao_manager/dto/DadosProdutoSaidaDTO.java
+++ src/main/java/com/deposito/construcao_manager/dto/DadosProdutoSaidaDTO.java
@@ -3,22 +3,22 @@ package com.deposito.construcao_manager.dto;
 import java.math.BigDecimal;
 
 import com.deposito.construcao_manager.domain.Produto;
-import lombok.Data;
 
-@Data
-public class DadosProdutoSaidaDTO {
 
-    private Long id;
-    private String nome;
-    private BigDecimal preco;
-    private Integer quantidadeEstoque;
+public record DadosProdutoSaidaDTO(
+
+    Long id,
+    String nome,
+    BigDecimal preco,
+    Integer quantidadeEstoque
+) {
 
     public static DadosProdutoSaidaDTO from(Produto produtoSalvo) {
-        DadosProdutoSaidaDTO produto_final = new DadosProdutoSaidaDTO();
-        produto_final.setId(produtoSalvo.getId());
-        produto_final.setNome(produtoSalvo.getNome());
-        produto_final.setPreco(produtoSalvo.getPreco());
-        produto_final.setQuantidadeEstoque(produtoSalvo.getQuantidadeEstoque());
-        return produto_final;
+        return new DadosProdutoSaidaDTO(
+            produtoSalvo.getId(),
+            produtoSalvo.getNome(),
+            produtoSalvo.getPreco(),
+            produtoSalvo.getQuantidadeEstoque()
+        );
     }
 }
